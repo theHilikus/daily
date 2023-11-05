25f1b1a2bbff51192d54e3bd22082843cbb107f7
Alejandro Endo <theHilikus@gmail.com>
2023-11-05 00:39:47 -0400

    Create calendar event widget
    Create a demo app for it too

---
+++ event_test/event_demo.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+	"github.com/theHilikus/daily/internal/ui"
+)
+
+func main() {
+	app := app.New()
+	window := app.NewWindow("Event Demo")
+	window.Resize(fyne.NewSize(400, 600))
+
+	details := widget.TextSegment{
+		Text: "Details",
+	}
+
+	title := ui.NewClickableText("hello", fyne.TextStyle{Bold: true}, color.Black)
+	button1 := widget.NewButton("but1", func() { fmt.Println("button1") })
+	button2 := widget.NewButton("but2", func() { fmt.Println("button2") })
+
+	sample := ui.NewEvent(title, []*widget.Button{button1, button2}, widget.NewRichText(&details))
+	window.SetContent(sample)
+	window.ShowAndRun()
+}
---
+++ internal/ui/event.go
@@ -0,0 +1,62 @@
+package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Event struct {
+	widget.BaseWidget
+
+	Title        *ClickableText
+	TitleButtons []*widget.Button
+	Detail       fyne.CanvasObject
+	open         bool
+	container    *fyne.Container
+}
+
+func NewEvent(title *ClickableText, titleButtons []*widget.Button, detail fyne.CanvasObject) *Event {
+	titleBox := container.NewHBox(title, layout.NewSpacer())
+	for _, button := range titleButtons {
+		titleBox.Add(button)
+	}
+
+	detail.Hide()
+	rootContainer := container.NewVBox(titleBox, detail)
+	result := &Event{
+		Title:        title,
+		TitleButtons: titleButtons,
+		Detail:       detail,
+		open:         false,
+		container:    rootContainer,
+	}
+	result.ExtendBaseWidget(result)
+
+	title.TapHandler = func(pe *fyne.PointEvent) {
+		if result.open {
+			result.Close()
+		} else {
+			result.Open()
+		}
+	}
+
+	return result
+}
+
+func (event *Event) Close() {
+	event.open = false
+	event.Detail.Hide()
+	event.Refresh()
+}
+
+func (event *Event) Open() {
+	event.open = true
+	event.Detail.Show()
+	event.Refresh()
+}
+
+func (event *Event) CreateRenderer() fyne.WidgetRenderer {
+	return widget.NewSimpleRenderer(event.container)
+}
