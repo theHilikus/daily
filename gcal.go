cbb9550fa08d16eb924821984e82ddf6b540b3b8
Alejandro Endo <theHilikus@gmail.com>
2023-11-11 14:16:56 -0500

    Retrieve events from gcal

---
+++ gcal.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"os"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+const (
+	tokenFile        = "gcalToken.json"
+	clientSecretFile = "secrets/client.json"
+)
+
+type googleCalendar struct {
+	service      *calendar.Service
+	eventsBuffer []event
+	requestStartDate time.Time
+	requestEndDate time.Time
+}
+
+func newGoogleCalendar() (*googleCalendar, error) {
+	result := googleCalendar{}
+
+	clientSecret, err := os.ReadFile(clientSecretFile)
+	if err != nil {
+		slog.Error("Unable to read client secret file: ", err)
+		return nil, err
+	}
+
+	config, err := google.ConfigFromJSON(clientSecret, calendar.CalendarEventsReadonlyScope)
+	if err != nil {
+		slog.Error("Unable to parse client secret file to config: %v", err)
+		return nil, err
+	}
+
+	tok := &oauth2.Token{}
+	tokenReader := strings.NewReader(preferences.String("calendar-token"))
+	err = json.NewDecoder(tokenReader).Decode(tok)
+	if err != nil {
+		slog.Error("Error decoding token")
+		return nil, err
+	}
+
+	client := config.Client(context.Background(), tok)
+
+	ctx := context.Background()
+	result.service, err = calendar.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		slog.Error("Unable to retrieve Calendar client: %v", err)
+	}
+
+	return &result, nil
+}
+
+func (gcal googleCalendar) getEvents(day time.Time) ([]event, error) {
+
+	if len(gcal.eventsBuffer) == 0 {
+		slog.Debug("Events buffer is empty")
+		err := gcal.retrieveEventsAround(day)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	const minBufferThreshold = 2
+
+	if int(day.Sub(gcal.requestStartDate).Hours()/24) < minBufferThreshold {
+		slog.Debug("Too close to buffer start")
+		err := gcal.retrieveEventsAround(gcal.requestStartDate)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if int(gcal.requestEndDate.Sub(day).Hours()/24) < minBufferThreshold {
+		slog.Debug("Too close to buffer end")
+		err := gcal.retrieveEventsAround(gcal.requestEndDate)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var result []event
+	for _, event := range gcal.eventsBuffer {
+		eventYear, eventMonth, eventDay := event.start.Date()
+		
+
+		if year, month, day2 := day.Date(); year == eventYear && month == eventMonth && day2 == eventDay{
+			result = append(result, event)
+		}
+	}
+
+	return result, nil
+}
+
+func (gcal *googleCalendar) retrieveEventsAround(day time.Time) error {
+	_, timezoneOffset := day.Zone()
+	const requestHalfWindow int = 4
+	gcal.requestStartDate = day.AddDate(0, 0, -requestHalfWindow).Truncate(24 * time.Hour).Add(time.Second * time.Duration(-timezoneOffset))
+	gcal.requestEndDate = day.AddDate(0, 0, requestHalfWindow).Truncate(24 * time.Hour).Add(time.Second * time.Duration(-timezoneOffset))
+	slog.Info("Retrieving events between " + gcal.requestStartDate.Format(time.RFC3339) + " and " + gcal.requestEndDate.Format(time.RFC3339))
+	response, err := gcal.service.Events.List(preferences.String("calendar-id")).SingleEvents(true).TimeMin(gcal.requestStartDate.Format(time.RFC3339)).TimeMax(gcal.requestEndDate.Format(time.RFC3339)).OrderBy("startTime").Do()
+	if err != nil {
+		slog.Error("Unable to retrieve events from google:", err)
+		return err
+	}
+
+	var allEvents []event
+	for _, item := range response.Items {
+		if item.Start.DateTime != "" {
+			//for now, ignore day events
+			eventStart, err := time.Parse(time.RFC3339, item.Start.DateTime)
+			if err != nil {
+				return err
+			}
+			
+			eventEnd, err := time.Parse(time.RFC3339, item.End.DateTime)
+			if err != nil {
+				return err
+			}
+			allEvents = append(allEvents, event{
+				title: item.Summary,
+				start: eventStart,
+				end: eventEnd,
+			})
+		}
+	}
+	gcal.eventsBuffer = allEvents
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,15 @@ go 1.21.3
 
 replace fyne.io/fyne/v2 => ../fyne/
 
-require fyne.io/fyne/v2 v2.4.1
+require (
+	fyne.io/fyne/v2 v2.4.1
+	golang.org/x/oauth2 v0.13.0
+	google.golang.org/api v0.149.0
+)
 
 require (
+	cloud.google.com/go/compute v1.23.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	fyne.io/systray v1.10.1-0.20230722100817-88df1e0ffa9a // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fredbi/uri v1.0.0 // indirect
@@ -19,6 +25,12 @@ require (
 	github.com/go-text/render v0.0.0-20230619120952-35bccb6164b8 // indirect
 	github.com/go-text/typesetting v0.0.0-20230616162802-9c17dd34aa4a // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -27,11 +39,17 @@ require (
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
 	github.com/yuin/goldmark v1.5.5 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
 )
