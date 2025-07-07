a2b3af873782e16cb32b35e5d13bd10a8db0cfbf
Alejandro Endo <theHilikus@gmail.com>
2025-07-07 18:43:10 -0400

    Use a reminder toast notification so that it stays on the screen
    Code mostly copied from https://github.com/go-toast/toast (MIT) since that library doesn't support scenario attribute in the toast element

--- go.mod
+++ go.mod
@@ -11,7 +11,6 @@ require (
 	github.com/zalando/go-keyring v0.2.6
 	golang.org/x/oauth2 v0.30.0
 	google.golang.org/api v0.237.0
-	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2
 )
 
 require (
--- internal/notification/notification_windows.go
+++ internal/notification/notification_windows.go
@@ -3,38 +3,153 @@
 package notification
 
 import (
+	"bytes"
+	"fyne.io/fyne/v2"
+	"io/ioutil"
 	"log/slog"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"text/template"
 
-	"fyne.io/fyne/v2"
-	"gopkg.in/toast.v1"
+	"github.com/nu7hatch/gouuid"
+	"syscall"
 )
+
+var toastTemplate *template.Template
+
+type notification struct {
+	// The name of your app. This value shows up in Windows 10's Action Centre, so make it
+	// something readable for your users. It can contain spaces, however special characters
+	// (eg. é) are not supported.
+	AppID string
+
+	// The main title/heading for the toast notification.
+	Title string
+
+	// The single/multi line message to display for the toast notification.
+	Message string
+
+	// An optional path to an image on the OS to display to the left of the title & message.
+	Icon string
+
+	// The type of notification level action (like toast.Action)
+	ActivationType string
+
+	// The activation/action arguments (invoked when the user clicks the notification)
+	ActivationArguments string
+
+	// Optional action buttons to display below the notification title & message.
+	Actions []action
+}
+
+type action struct {
+	Type      string
+	Label     string
+	Arguments string
+}
+
+func init() {
+	toastTemplate = template.New("toast")
+	toastTemplate.Parse(`
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+
+$APP_ID = '{{.AppID}}'
+$SOUND = "ms-winsoundevent:Notification.Reminder"
+
+$template = @"
+<toast scenario="reminder" activationType="{{.ActivationType}}" launch="{{.ActivationArguments}}">
+    <visual>
+        <binding template="ToastGeneric">
+            <image placement="appLogoOverride" src="{{.Icon}}" />
+            <text><![CDATA[{{.Title}}]]></text>
+            <text><![CDATA[{{.Message}}]]></text>
+        </binding>
+    </visual>
+    
+	<audio src="$SOUND" loop="false" />
+
+    {{if .Actions}}
+    <actions>
+        {{range .Actions}}
+        <action activationType="{{.Type}}" content="{{.Label}}" arguments="{{.Arguments}}" />
+        {{end}}
+    </actions>
+    {{end}}
+</toast>
+"@
+
+$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$xml.LoadXml($template)
+$toast = New-Object Windows.UI.Notifications.ToastNotification $xml
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($APP_ID).Show($toast)
+    `)
+}
 
 func SendNotification(app fyne.App, title, message string, meetingLink string) {
 	slog.Info("Using Windows-specific notification.")
 
-	var actions []toast.Action
+	var actions []action
 	if meetingLink != "" {
-		actions = []toast.Action{
+		actions = []action{
 			{Type: "protocol", Label: "Launch Meeting", Arguments: meetingLink},
 		}
 	}
-	notification := toast.Notification{
-		AppID:    app.Metadata().Name,
-		Title:    title,
-		Message:  message,
-		Audio:    toast.Reminder,
-		Actions:  actions,
-		Duration: toast.Long,
-		Icon:     dumpIcon(app),
+	notification := notification{
+		AppID:   app.Metadata().Name,
+		Title:   title,
+		Message: message,
+		Actions: actions,
+		Icon:    dumpIcon(app),
 	}
 
 	slog.Debug("Sending notification", "data", notification)
-	err := notification.Push()
+	err := notification.push()
 	if err != nil {
 		slog.Error("Toast Error:", "error", err)
 	}
+}
+
+func (n *notification) push() error {
+	n.applyDefaults()
+	xml, err := n.buildXML()
+	if err != nil {
+		return err
+	}
+	return invokeTemporaryScript(xml)
+}
+
+func (n *notification) applyDefaults() {
+	if n.ActivationType == "" {
+		n.ActivationType = "protocol"
+	}
+}
+
+func (n *notification) buildXML() (string, error) {
+	var out bytes.Buffer
+	err := toastTemplate.Execute(&out, n)
+	if err != nil {
+		return "", err
+	}
+	return out.String(), nil
+}
+
+func invokeTemporaryScript(content string) error {
+	id, _ := uuid.NewV4()
+	file := filepath.Join(os.TempDir(), id.String()+".ps1")
+	defer os.Remove(file)
+	err := ioutil.WriteFile(file, []byte(content), 0600)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("PowerShell", "-ExecutionPolicy", "Bypass", "-File", file)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	if err = cmd.Run(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func dumpIcon(app fyne.App) string {
