423c35d6729d8ec3e53a5fa58c857a3db9953116
Alejandro Endo <theHilikus@gmail.com>
2025-07-15 19:30:34 -0400

    Escape meeting URL

--- internal/notification/notification_windows.go
+++ internal/notification/notification_windows.go
@@ -14,6 +14,7 @@ import (
 	"text/template"
 	"unsafe"
 
+	"encoding/xml"
 	"syscall"
 )
 
@@ -95,7 +96,9 @@ func SendNotification(app fyne.App, title, message string, meetingLink string) {
 
 	actions := []action{}
 	if meetingLink != "" {
-		actions = append(actions, action{Type: "protocol", Label: "Launch Meeting", Arguments: meetingLink})
+		buf := &bytes.Buffer{}
+		xml.EscapeText(buf, []byte(meetingLink))
+		actions = append(actions, action{Type: "protocol", Label: "Launch Meeting", Arguments: buf.String()})
 	}
 	actions = append(actions, action{Type: "protocol", Label: "Dismiss"})
 
